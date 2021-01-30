7297cd9725da0ef8b492e0dc9ab7b1d5d90bf36e
VictrixHominum <jchhollingworth97@gmail.com>
2021-01-30 19:25:19 +0000

    Added department method

--- src/Email.java
+++ src/Email.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Email {
     private String firstName;
     private String lastName;
@@ -6,16 +8,26 @@ public class Email {
     private int mailboxCapacity;
     private String alternateEmail;
 
-    // Constructor to recieve first and last name
+    // Constructor to receive first and last name
 
     public Email(String firstname, String lastname) {
         this.firstName = firstname;
         this.lastName = lastname;
         System.out.println("Email Created: " + this.firstName + " " + this.lastName);
+        //Call a method asking for a department and return a department
+        this.department = setDepartment();
     }
 
-    // ASk for department
-
+    // Ask for department
+    private String setDepartment(){
+        System.out.print("Please input employee's department: \n1 for Sales \n2 for Development \n3 for Accounting \n0 for None");
+        Scanner in  = new Scanner(System.in);
+        int depChoice = in.nextInt();
+        if (depChoice == 1) {return "Sales";}
+        else if (depChoice ==2) {return "Development";}
+        else if (depChoice ==3) {return "Accounting";}
+        else {return "";}
+    }
 
     //Generate a random password
 
