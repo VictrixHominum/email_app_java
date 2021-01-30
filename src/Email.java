77b686d0d7336487930464713556c0240d9ccefe
VictrixHominum <jchhollingworth97@gmail.com>
2021-01-30 19:19:34 +0000

    Constructor created

--- src/Email.java
+++ src/Email.java
@@ -1,13 +1,18 @@
 public class Email {
-    private String firstname;
-    private String lastname;
+    private String firstName;
+    private String lastName;
     private String password;
     private String department;
     private int mailboxCapacity;
     private String alternateEmail;
 
     // Constructor to recieve first and last name
-    
+
+    public Email(String firstname, String lastname) {
+        this.firstName = firstname;
+        this.lastName = lastname;
+        System.out.println("Email Created: " + this.firstName + " " + this.lastName);
+    }
 
     // ASk for department
 
