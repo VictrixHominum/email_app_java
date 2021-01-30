b42886ebcc0222b43578d279dd9831b1cc2577b3
VictrixHominum <jchhollingworth97@gmail.com>
2021-01-30 19:16:02 +0000

    Initial commit

---
+++ src/Email.java
@@ -0,0 +1,25 @@
+public class Email {
+    private String firstname;
+    private String lastname;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    private String alternateEmail;
+
+    // Constructor to recieve first and last name
+    
+
+    // ASk for department
+
+
+    //Generate a random password
+
+
+    //Set mailbox capacity
+
+
+    //Set alternate email
+
+
+    //Change the password
+}
